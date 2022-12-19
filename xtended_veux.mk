7f07fa7e43cf751e3f901e7ac6cbe9a808e0c086
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2022-12-19 20:05:47 +0530

    Update and rename lineage_veux.mk to xtended_veux.mk

--- xtended_veux.mk
+++ xtended_veux.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common riceDroid Stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xtended/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_veux
@@ -22,16 +22,7 @@ PRODUCT_MODEL := POCO X4 Pro 5G
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# riceDroid Stuff with GApps
+# Xtended Stuff with GApps
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_BUILD_GRAPHENEOS_CAMERA := true
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_USE_PIXEL_FINGERPRINT := true
-SUSHI_BOOTANIMATION  := 1080
 TARGET_ENABLE_BLUR := true
-WITH_GMS := true
-
-# Maintainer Stuff
-RICE_MAINTAINER := SharmaG
-RICE_OFFICIAL := true
+WITH_GAPPS := true
